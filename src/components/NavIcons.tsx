df63b20aef93cdb61802f849ce922dfc4f25723a
dinhduc24k322 <dinhducphan02@gmail.com>
2024-09-24 20:31:21 +0700

    Change Nav Icons

--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -1,67 +1,62 @@
 "use client";
 
-import { useRouter } from 'next/navigation';
-import React, { useState } from 'react'
-import Image from 'next/image';
-import Link from 'next/link';
-import CartModal from './CartModal';
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import CartModal from "./CartModal";
 
 const NavIcons = () => {
-    const [isProfileOpen, setIsProfileOpen] = useState(false);
-    const [isCartOpen, setIsCartOpen] = useState(false);
+  const [isProfileOpen, setIsProfileOpen] = useState(false);
+  const [isCartOpen, setIsCartOpen] = useState(false);
 
-    const router = useRouter();
+  const router = useRouter();
 
-    //TEMPORARY
-    const isLoggedIn = false;
+  //TEMPORARY
+  const isLoggedIn = false;
 
-    const handleProfile = () => {
-        if(!isLoggedIn) {
-            router.push("/login");
-        }
-        setIsProfileOpen((prev) => !prev);
-    };
+  const handleProfile = () => {
+    if (!isLoggedIn) {
+      router.push("/login");
+    }
+    setIsProfileOpen((prev) => !prev);
+  };
 
   return (
-    <div className='flex items-center gap-4 xl:gap-6 relative'>
-        <Image 
-            src="/profile.png" 
-            alt='' 
-            width={22} 
-            height={22} 
-            className='cursor-pointer'
-            onClick={handleProfile}
-        />
-        {isProfileOpen && (
-            <div className="absolute p-4 rounded-md top-12 left-0 text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20">
-                <Link href="/">Profile</Link>
-                <div className="mt-2 cursor-pointerz">Logout</div>
-            </div>
-        )}
-        <Image 
-            src="/notification.png"
-            alt='' 
-            width={22} 
-            height={22} 
-            className='cursor-pointer'
-        />
-        <div className='relative cursor-pointer'>
-
-        <Image 
-            src="/cart.png" 
-            alt='' 
-            width={22} 
-            height={22} 
-            className='cursor-pointer'
-            onClick={() => setIsCartOpen((prev) => !prev)}
-            />
-            <div className='absolute -top-4 -right-4 w-6 h-6 bg-vintage rounded-full text-white text-sm flex items-center justify-center'>2</div>
+    <div className="flex items-center gap-4 xl:gap-6 relative">
+      <Image
+        src="/profile.png"
+        alt=""
+        width={22}
+        height={22}
+        className="cursor-pointer"
+        onClick={handleProfile}
+      />
+      {isProfileOpen && (
+        <div className="absolute p-4 rounded-md top-12 left-0 text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20">
+          <Link href="/">Profile</Link>
+          <div className="mt-2 cursor-pointerz">Logout</div>
         </div>
-        {isCartOpen && (
-            <CartModal/>
-        )}
+      )}
+      <Image
+        src="/notification.png"
+        alt=""
+        width={22}
+        height={22}
+        className="cursor-pointer"
+      />
+      <div
+        className="relative cursor-pointer"
+        onClick={() => setIsCartOpen((prev) => !prev)}
+      >
+        <Image src="/cart.png" alt="" width={22} height={22} />
+        <div className="absolute -top-4 -right-4 w-6 h-6 bg-vintage rounded-full text-white text-sm flex items-center justify-center">
+          2
+        </div>
+      </div>
+      {isCartOpen && <CartModal />}
     </div>
-  )
-}
+  );
+};
 
-export default NavIcons
+export default NavIcons;
